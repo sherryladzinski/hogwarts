3588e3dddba77bde807a8c26cad1350db4d041a2
Sherry Ladzinski <sherryladzinski@gmail.com>
2015-02-19 15:30:05 -0500

    fixed houses index and students index pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Hogwarts::Application.routes.draw do
+resources :houses, only: [:index, :show] 
+resources :students
  
-root "house#index"
-resource :house, only: [:index, :show]
-resources :students, only: [:index, :show]
+root "houses#index"
+
 
 end
